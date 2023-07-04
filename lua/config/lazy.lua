d5af5d9a88366836db3f9f6451ffcf290df50540
zhanghaojie <1455123087@qq.com>
2023-07-04 13:41:18 +0800

    feat: autoformat on save: false

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,10 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    {
+      "neovim/nvim-lspconfig",
+      opts = { autoformat = false }, -- add this line
+    },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
