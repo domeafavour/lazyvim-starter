90b3393793027e4af6260458aae490bc078cfd1e
zhanghaojie <1455123087@qq.com>
2023-07-18 22:06:38 +0800

    feat: colorscheme `NeoSolarized`

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,10 +2,13 @@ return {
   "EdenEast/nightfox.nvim",
   "romainl/flattened",
   "Mofiqul/vscode.nvim",
+  "sainnhe/sonokai",
+  "lifepillar/vim-solarized8",
+  "Tsuzat/NeoSolarized.nvim",
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vscode",
+      colorscheme = "NeoSolarized",
     },
   },
 }
