56bf7458ba93ee924f1b61b7571b1af4fbf161ae
zhanghaojie <1455123087@qq.com>
2024-04-23 10:46:31 +0800

    feat: change colorscheme to 'tokyonight-night'

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,8 @@
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = false,
-      terminal_colors = true,
-      enable_italics = false,
-    },
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "tokyonight-night",
     },
   },
 }
